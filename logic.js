6387ba36ed634efd17dae68a228bfa7dfc3f3d4c
Jovan Krnajski Belovljev <hosze023@gmail.com>
2024-02-25 16:58:00 +0100

    fas

--- logic.js
+++ logic.js
@@ -1,8 +1,9 @@
+
 function search()
 {
     let fsearch = prompt("Please write your search down below:","");
     if(fsearch!=null && fsearch!="")
     window.confirm("Thank you for searching!");
     else
-    window.confirm("Sorry, but you didn't searched for anything.");
+    window.confirm("Sorry, but you didn't search for anything.");
 }
